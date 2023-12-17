42e7fd7636261eaa4b6e6afd0fe38863c92d0ce8
bykytadev <bykyta@outlook.com.vn>
2023-12-17 16:48:40 +0700

    first commit + config application.yml

---
+++ src/main/java/com/project/tcarshop/TcarshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.tcarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcarshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcarshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tcarshop/TcarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tcarshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
